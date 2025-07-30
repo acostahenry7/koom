1b4979d91d46f8f1b3f76bc088f4a1c291e9120f
acostahenry7 <73194577+acostahenry7@users.noreply.github.com>
2025-07-30 17:53:52 -0400

    starting-with-home-headings

--- app/(root)/(home)/history/page.tsx
+++ app/(root)/(home)/history/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const History = () => {
-  return <div>History</div>;
+  return (
+    <div>
+      <h1 className="heading">History</h1>
+    </div>
+  );
 };
 
 export default History;
